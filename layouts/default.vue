019bd2529b8f8bb3d07329e27c4255a3635e27fe
return-nil <whytaro@gmail.com>
2020-01-02 09:58:13 +0900

    ga

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,3 @@
     </main>
   </div>
 </template>
-
-<script>
-  const Vue = require('vue')
-  const VueAnalytics = require('vue-analytics').default
-
-  Vue.use(VueAnalytics, {
-    id: 'UA-155171593-1'
-  })
-</script>
